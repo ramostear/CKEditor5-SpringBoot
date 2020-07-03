53a3fa9ed588cc7b38dac0bbd3f092adbb7dc885
ramostear <ramostear@163.com>
2020-07-04 03:45:19 +0800

    Create and initialize CKEditor5-SpringBoot project

---
+++ src/main/java/com/ramostear/ckeditor/Ckeditor5SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.ramostear.ckeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ckeditor5SpringbootApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ckeditor5SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ramostear/ckeditor/Ckeditor5SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramostear.ckeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ckeditor5SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
